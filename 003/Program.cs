d6e569822f981d04c099fd8c0febe151b72383b8
Natalia <zhuravlevanyu@mail.ru>
2023-01-22 02:10:02 +0300

    gitinore

---
+++ 003/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+int Prompt(string message)
+{
+    Console.Write($"{message}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int x = Prompt("Введите число N: ");
+//Math.Pow(x, 3);// где Х - наша переменная, а 3 - степень, в которую возводим
+int i = 1;
+while (i <= x)
+{
+    double r = Math.Pow(i, 3);
+    System.Console.WriteLine($"Кубы числа от 1 до {i} равен " + r);
+    i++;
+}
